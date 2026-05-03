e3047040fda496d4eae34746acb4a430c61a0f9e
Elene Gotsiridze <elene.gociridze@gmail.com>
2026-05-03 23:20:11 +0400

    completed reversestring exericise

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(stringToReverse) {
+    let array = stringToReverse.split("")
+    let reversedString = array.reverse()
+    return reversedString.join("")
 };
 
 // Do not edit below this line
