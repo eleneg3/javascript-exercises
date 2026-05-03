49468b6dd6989f234af5ef27d46c5fc14691ce5b
Elene Gotsiridze <elene.gociridze@gmail.com>
2026-05-03 23:59:30 +0400

    finished remove from array exercise

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, thingsToRemove) {
+    let thingsToRemoveArray = Array.from(arguments)
+    let newArray = arr.filter(item => !thingsToRemoveArray.includes(item))
+    return newArray
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
