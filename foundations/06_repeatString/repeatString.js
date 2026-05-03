d25ddccfc9e9d245995f5e0301d8abd874763502
Elene Gotsiridze <elene.gociridze@gmail.com>
2026-05-03 23:11:40 +0400

    competed string repeat exercise

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+let errorMessage = "ERROR"
+const repeatString = function(stringToRepeat, num) {
+    let repeatedString = ""
+    if (num < 0) {
+        return errorMessage
+    } else {
+        for (let i = 0; i < num; i++) {
+            repeatedString = repeatedString + stringToRepeat
+        }
+        return repeatedString 
+    }
 };
 
 // Do not edit below this line
