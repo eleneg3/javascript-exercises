1e9e51b475e9140833fe04be7200fec9e3e56b00
Elene Gotsiridze <elene.gociridze@gmail.com>
2026-05-04 16:03:07 +0400

    completed f to c exercise

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let convertedValue = (fahrenheit - 32) * 5/9
+  let roundedValue = Math.round(convertedValue * 10) / 10
+  return roundedValue
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let convertedValue = (celsius * 9/5) + 32
+  let roundedValue = Math.round(convertedValue * 10) / 10
+  return roundedValue
 };
 
 // Do not edit below this line
