2ff9bd0fb5d5382b5476caba7a0f9362dee49418
Elene Gotsiridze <elene.gociridze@gmail.com>
2026-05-04 13:20:17 +0400

    finished sum all exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(numOne, numTwo) {
+    let numberArray = [] 
+    let errorMessage = "ERROR"
+    if (!Number.isInteger(numOne) || !Number.isInteger(numTwo) || numOne <=0 || numTwo <= 0) {
+        return errorMessage
+    } else if (numOne < numTwo) {
+        for (let i = numOne; i <= numTwo; i++) {
+            numberArray.push(i)
+        }
+        return numberArray.reduce((acc, current) => acc + current, 0)
+    } else if (numTwo < numOne) {
+        let numberArray = []
+        for (let i = numTwo; i <= numOne; i++) {
+            numberArray.push(i)
+        }
+        return numberArray.reduce((acc, current) => acc + current, 0)
+    }  
 };
 
 // Do not edit below this line
